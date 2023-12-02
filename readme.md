0d4b225301a36c0140632cc83b04bbc5d39e6693
kavipriya2912002 <kavipriya2912002@gmail.com>
2023-12-02 22:55:24 +0530

    Code Updated

--- readme.md
+++ readme.md
@@ -1,1 +1,1 @@
-Using Promise to retrive and display the University List in United Kingdom from an api.
+Using Promise to retrive and display the Dogs images from an api.
--- script.js
+++ script.js
@@ -1,42 +1,34 @@
-const container = document.getElementsByClassName("container")[0];
 
-function countryDetails() {
-  const url = "http://universities.hipolabs.com/search?country=United+Kingdom";
+var container=document.createElement("div")
+container.className="dog";
 
-  //!Fetch and Promise
-  fetch(url)
-    .then((data) => data.json())
-    .then((countries) => {
-      
+var row=document.createElement("div");
+row.className="row";
 
-      //! forEach loop is used to map data
-      countries.forEach((countrys) => {
-        const division = document.createElement("div");
-        division.setAttribute("class", "row");
-        division.innerHTML = `<div class="col-sm-6 col-md-4 col-lg-4 col-xl-4">
-                                      <div class="card h-100">
-                                          <div class="card-header">UK Universities Information </div>
-                                          
-                                          <div class="card-body">
-                                              <div class="card-text">Country Name: ${countrys.country}</div> <br />
-                                              <div class="card-text">University Name: ${countrys.name}</div> <br />
-                                              <div class="card-text">Alpha code: ${countrys.alpha_two_code}</div> <br />
-                                              <div class="card-text">Web Page Reference:<a href='${countrys.web_pages}'>View Web page</a> </div> <br />
-                                              <div class="card-text">Domains: ${countrys.domains}</div> <br />
-                                       
-                                          </div>
-                                      </div>
-                                  </div>
-                                  <br /> 
-                                  <br />`;
-        container.appendChild(division);
-      });
-    })
-    .catch((error) => console.log(error));
+container.append(row)
+
+//Fetching data 
+var res=fetch("https://dog.ceo/api/breed/hound/images? limit=30");
+res.then((data)=>data.json())
+.then((data1)=>dogs(data1))
+.catch((error)=>console.log(error));
+
+function dogs(data1){
+
+
+    for(let i=0; i<data1.message.length; i++){
+        row.innerHTML +=`
+        <div class="col-lg-3 col-sm-12">
+        
+        <div class="design"><img src="${data1.message[i]}" alt="..."> <br>
+        <button type="button" class="btn btn-outline-success">Download</button>
+        </div>
+        <div class="card-body">
+       
+    </div>
+</div>      `
+
+
+    }
+    document.body.append(container)
 }
-
-//!Function call
-countryDetails();
-
-
-
